a4cdcd42459b51d39194dc44d57ba4c686c45cf1
Simon <schauppinger@gmail.com>
2019-08-23 18:40:41 +0200

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
+    <div id="app">
+      <div class="content">
         <app-header></app-header>
         <router-view></router-view>
+      </div>
         <app-footer></app-footer>
     </div>
 </template>
@@ -9,33 +11,19 @@
 <script>
 import NavbarComp from './components/NavbarComp.vue'
 import AppFooter from './components/AppFooter.vue'
-import gql from 'graphql-tag'
-
-const getRunsQuery = gql`
-  query getRuns {
-    getRuns{
-      id
-      time
-    }
-  }
-`;
 
 export default {
   name: 'app',
   components: {
     'app-header': NavbarComp,
     'app-footer': AppFooter
-  },
-  data: () => {
-  },
-  apollo: {
-    getRuns: {
-      query: getRunsQuery
-    }
   }
 }
 </script>
 
 <style>
-
+.content {
+    min-height: calc(100vh - 112px);
+    /* 80px header + 40px footer = 120px  */
+}
 </style>
